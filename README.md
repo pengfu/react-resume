fa4cf666389001f32eb34b388b69dcc326fe7b62
chang_su <chang_su@hunliji.com>
2017-05-15 17:14:57 +0800

    first demo commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This project is Established on the guide of https://scotch.io/tutorials/setup-a-react-environment-using-webpack-and-babel.
---
+++ app/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by chang_su on 2017/5/15.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('root'));
