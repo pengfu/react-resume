9b00f201d1126a7f3dd6e8d9c3c1b28502fb76dd
chang_su <chang_su@hunliji.com>
2017-05-25 10:55:22 +0800

    travis test3

--- deploy.sh
+++ deploy.sh
@@ -1,54 +1,28 @@
 #!/bin/bash
-set -e # Exit with nonzero exit code if anything fails
+set -o errexit -o nounset
 
-SOURCE_BRANCH="master"
-TARGET_BRANCH="gh-pages"
-
-function doCompile {
- # ./compile.sh
- echo "doCmpile"
-}
-
-# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "Skipping deploy; just doing a build."
-    doCompile
-    exit 0
+if [ "$TRAVIS_BRANCH" != "master" ]
+then
+  echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
+  exit 0
 fi
 
-# Save some useful information
-REPO=`git config remote.origin.url`
-SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
-SHA=`git rev-parse --verify HEAD`
+rev=$(git rev-parse --short HEAD)
 
-# Clone the existing gh-pages for this repo into out/
-# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO out
-cd out
-git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
-cd ..
+cd stage/_book
 
-# Clean out existing contents
-rm -rf out/**/* || exit 0
-
-# Run our compile script
-doCompile
-
-# Now let's go have some fun with the cloned repo
-cd out
+git init
 git config user.name "pengfu"
 git config user.email "pengfoo@126.com"
 
-# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if git diff --quiet; then
-    echo "No changes to the output on this push; exiting."
-    exit 0
-fi
+git remote add upstream "https://$GH_TOKEN@github.com/react-resume.git"
+git fetch upstream
+git reset upstream/gh-pages
 
-# Commit the "changes", i.e. the new version.
-# The delta will show diffs between new and old versions.
+echo "pengfoo@126.com" > CNAME
+
+touch .
+
 git add -A .
-git commit -m "Deploy to GitHub Pages: ${SHA}"
-
-# Now that we're all set up, we can push.
-git push $SSH_REPO $TARGET_BRANCH
+git commit -m "rebuild pages at ${rev}"
+git push -q upstream HEAD:gh-pages
