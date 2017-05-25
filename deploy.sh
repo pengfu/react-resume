17b0c25e661e6ceb16dc79c5d8bf32e4c9cc3ddf
chang_su <chang_su@hunliji.com>
2017-05-25 10:48:32 +0800

    travis test2

--- deploy.sh
+++ deploy.sh
@@ -36,8 +36,8 @@ doCompile
 
 # Now let's go have some fun with the cloned repo
 cd out
-git config user.name "chang_su"
-git config user.email "chang_su@hunliji.com"
+git config user.name "pengfu"
+git config user.email "pengfoo@126.com"
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
 if git diff --quiet; then
@@ -49,16 +49,6 @@ fi
 # The delta will show diffs between new and old versions.
 git add -A .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
-
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out ../deploy_key -d
-chmod 600 ../deploy_key
-eval `ssh-agent -s`
-ssh-add deploy_key
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
